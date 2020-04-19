50cbe6e866ba1983080829ac29e3352ed57fdc4a
Sam Scott <samscott497@gmail.com>
2020-04-19 02:07:51 -0400

    Updated InkTankUI in new test scene

--- IsometricSplatoon/Player/UI/InkTankUI.gd
+++ IsometricSplatoon/Player/UI/InkTankUI.gd
@@ -1,11 +1,16 @@
 extends NinePatchRect
 
-onready var inkBar = $MarginContainer/TextureProgress
+onready var inkBar = $TextureProgress
 #onready var noInk = $MarginContainer/NoInkTexture
 #onready var inkRecover = $MarginContainer/RecoverInkTexture
 
+func _ready():
+	hide()
+
 func updateInkUI(ink_amount):
 	inkBar.value = (inkBar.max_value - inkBar.min_value) * ink_amount
+	$FadeTimer.start()
+	show()
 
 func _on_TempPlayer_ink_use(ink_left):
 	updateInkUI(ink_left)
@@ -16,4 +21,10 @@ func _on_TempPlayer_ink_recover(ink_left):
 		$AnimationPlayer.play("ink_recover")
 
 func _on_TempPlayer_ink_fail():
+	$FadeTimer.start(1)
+	show()
 	$AnimationPlayer.play("no_ink")
+
+
+func _on_FadeTimer_timeout():
+	hide()
