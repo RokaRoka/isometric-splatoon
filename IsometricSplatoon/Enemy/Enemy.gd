9cc2c897b5e89e9d1d32a75f3d4ba2d772ef5b92
samjs <samscott497@gmail.com>
2020-05-25 23:48:26 -0400

    Enemy tscn structure nearly done

--- IsometricSplatoon/Enemy/Enemy.gd
+++ IsometricSplatoon/Enemy/Enemy.gd
@@ -1,4 +1,4 @@
-extends KinematicBody2D
+extends Node2D
 
 #external node refs
 onready var inkManager = get_node( "/root/Game/InkManager")
@@ -6,36 +6,34 @@ onready var inkManager = get_node( "/root/Game/InkManager")
 var velocity  = Vector2()
 var ground = GroundType.None
 
-#ink stuff
-var inkTank = 1.0
-var inkRecovering = false
-var inkRecoverRestartCount = 0 #this one counts
-var inkRecoverRestartTime = 1.5 #this is the num that has to be reached to restart
-
 #state vars
 var states_stack = []
 var current_state = null
 
 
-var health = 75
+var health = 175
 
 func _ready():
 	$AnimationPlayer.play("float")
 
 func takeDamage(dmgAmount):
-#	#particle effect!
+	#particle effect!
 #	$HitParticle.restart()
 #	$HitParticle.emitting = true
-#	#sound
+#	
+	#sound
 #	$AudioStreamPlayer.PlayAudio("hit")
+	
 	#damage
 	health -= dmgAmount
-#	if health <= 0:
+	if health <= 0:
+		$Hitbox.monitoring = false
+		$Hitbox.monitorable = false
+		queue_free()
 #		$AnimationPlayer.play("pop")
 #		emit_signal("popped")
 
-func _on_Balloony_body_entered(body):
-	#print("body name is "+body.name)
+func _on_body_entered(body):
 	if body.is_in_group("bullet"):
 		var bullet = body
 		takeDamage(bullet.damageDirect)
